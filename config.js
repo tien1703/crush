f901c9ae0f9815ed68585a6c7ad913ab4c9d6c90
tien1703 <85288764+tien1703@users.noreply.github.com>
2021-06-03 17:53:48 +0700

    Create config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Ê ! Con lợn Link à!',
+    introTitle: 'Em yêu à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
